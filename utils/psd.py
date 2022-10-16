dc7047cdf8dc78d681c1f513d1051c70d65b4665
simana_sch <tktossi@live.com>
2022-10-16 22:41:47 +0900

    オブジェクトの追加順修正、画像にfake-user設定を追加

--- utils/psd.py
+++ utils/psd.py
@@ -17,38 +17,53 @@ def get_file_name_from_path(filepath):
 def get_image_as_np_array(image):
   return np.array(image).flatten() / 255
 
+
+def get_layer_absolute_path(layer):
+  # 
+  if (layer.name == "Root" )| (layer.name == None) | (layer.parent == None):
+    return 'Root'
+  elif layer.parent.name == "Root":
+    if layer.is_group():
+      # Root階層のGroup
+      return layer.name
+    else:
+      # Root階層の通常Layer
+      return "Root"
+  else:
+    if layer.is_group():
+      # Root階層以外でGroupの場合
+      return os.path.join(get_layer_absolute_path(layer.parent),layer.name)
+    else:
+      # Root階層以外でlayerの場合
+      return get_layer_absolute_path(layer.parent)
+
 def add_images_from_psd(path, encoding='macroman'):
   # psdファイルを開く
   psd = PSDImage.open(os.path.abspath(path),encoding=encoding)
   rootPlane = add_group_layer(psd)
   rootPlane.name = get_file_name_from_path(path)
-  parent=rootPlane
+  parentNames = {get_layer_absolute_path(rootPlane):rootPlane}
 
   # psd.descendantsでループ
   for layer in list(psd.descendants()):
+    print(get_layer_absolute_path(layer))
     # pixelLayerなら、平面を追加
     if layer.kind == 'pixel':
       layerPlane = add_plane_of_layer_size(layer, psd)
       if layer.parent.name == 'Root':
-        layerPlane.parent = rootPlane
         global zIndex
-        zIndex += 0.001
-      else:
-        layerPlane.parent = parent
+        zIndex += 0.0001
     # groupならemptyObjectを追加、parentにする
     elif layer.kind=='group':
-      groupLayer = add_group_layer(layer)
-      if layer.parent.name == 'Root':
-        groupLayer.parent = rootPlane
-      else:
-        groupLayer.parent = parent
-      parent = groupLayer
+      layerPlane = add_group_layer(layer)
+      parentNames[get_layer_absolute_path(layer)] = layerPlane
+    layerPlane.parent = parentNames[get_layer_absolute_path(layer.parent)]
 
 
 def add_group_layer(psd):
   global pixel_rate
   global zIndex
-  zIndex += 0.001
+  zIndex += 0.0001
   if pixel_rate == 0.0:
     # 1pxあたり何メートルになるか出す
     pixel_rate = 1/max(psd.size)
@@ -103,6 +118,7 @@ def add_material(psdLayer):
   add_mat.node_tree.links.new(shader_node.inputs['Alpha'], imageTextureNode.outputs['Alpha'])
   # 表示画像を設定
   bpy_image = bpy.data.images.new(psdLayer.name, w, h, alpha=True)
+  bpy_image.use_fake_user = True
   bpy_image.pixels = psdLayer.numpy().flatten()
   imageTextureNode.image = bpy_image
   return add_mat
